10de46be892bb596cf507c4a1897ee930a7802a3
DM7935-GH <davidmuoneke45@gmail.com>
2025-01-23 16:01:26 +0000

    Added MergeSort tests, 23-01-25.

--- TestFolder/JUnit4_TestClassB.java
+++ TestFolder/JUnit4_TestClassB.java
@@ -51,4 +51,17 @@ public class JUnit4_TestClassB {
         assertArrayEquals(expectedArray3, SAObject.Quicksort(inputArray3));
         assertArrayEquals(expectedArray4, SAObject.Quicksort(inputArray4));
     }
+
+    @Test
+    public void Test_MergeSort() {
+        int[] inputArray1 = {4,3,7,6,1,2,9,5,8,0}; int[] expectedArray1 = {0,1,2,3,4,5,6,7,8,9};
+        int[] inputArray2 = {}; int[] expectedArray2 = {};
+        int[] inputArray3 = {-50}; int[] expectedArray3 = {-50};
+        int[] inputArray4 = {22,-22}; int[] expectedArray4 = {-22,22};
+        
+        assertArrayEquals(expectedArray1, SAObject.MergeSort(inputArray1));
+        assertArrayEquals(expectedArray2, SAObject.MergeSort(inputArray2));
+        assertArrayEquals(expectedArray3, SAObject.MergeSort(inputArray3));
+        assertArrayEquals(expectedArray4, SAObject.MergeSort(inputArray4));
+    }
 }
